48c524478a7d91bdf1018c86e752e059018ccc5b
wanghaisheng <edwin.uestc@gmail.com>
2022-04-10 05:32:35 +0800

    fix proxy

--- scrape.py
+++ scrape.py
@@ -36,8 +36,8 @@ def signalHandler(signal, frame):
 async def worker(id: int, st: datetime, ed: datetime, proxypool: str, delay: float, timeout: float) -> dict:
     workerRes = {}  # e.g. {'22.3.4.5': '2021-04-26 03:53:41'}
     # proxy = await popProxy(id, proxypool, timeout)
-    proxy = requests.get("https://{}".format(proxypool)).text()
-
+    proxy = requests.get(proxypool).text()
+    
     log.info('[{}] Thread starts: proxy={} st={} ed={}'.format(id, proxy, st, ed))
     item_list = []
     topic='genshin'
