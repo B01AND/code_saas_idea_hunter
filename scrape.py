cfaf3dc92eda5e8e5bd28595c162fb0eeaf99462
wanghaisheng <edwin.uestc@gmail.com>
2022-04-10 05:47:19 +0800

    fix

--- scrape.py
+++ scrape.py
@@ -36,6 +36,7 @@ def signalHandler(signal, frame):
 async def worker(id: int, st: datetime, ed: datetime, proxypool: str, delay: float, timeout: float) -> dict:
     workerRes = {}  # e.g. {'22.3.4.5': '2021-04-26 03:53:41'}
     # proxy = await popProxy(id, proxypool, timeout)
+    print('proxypool',proxypool)
     proxy = requests.get(proxypool).text()
     
     log.info('[{}] Thread starts: proxy={} st={} ed={}'.format(id, proxy, st, ed))
